b8fc35975165b0832abaf46d0e932c1a883198e7
151250128 <151250128@smail.nju.edu.cn>
2018-04-27 12:10:51 +0800

    <dev>:<basic contents of index page and result page>

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,8 @@
 var option = {
     title: {
-        text: '堆叠区域图'
+        text: '分数'
     },
-    tooltip : {
+    tooltip: {
         trigger: 'axis',
         axisPointer: {
             type: 'cross',
@@ -12,7 +12,7 @@ var option = {
         }
     },
     legend: {
-        data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
     },
     toolbox: {
         feature: {
@@ -25,50 +25,22 @@ var option = {
         bottom: '3%',
         containLabel: true
     },
-    xAxis : [
+    xAxis: [
         {
-            type : 'category',
-            boundaryGap : false,
-            data : ['周一','周二','周三','周四','周五','周六','周日']
+            type: 'category',
+            boundaryGap: false,
+            data: ['1', '2', '3', '4', '5', '6', '7']
         }
     ],
-    yAxis : [
+    yAxis: [
         {
-            type : 'value'
+            type: 'value'
         }
     ],
-    series : [
+    series: [
         {
-            name:'邮件营销',
-            type:'line',
-            stack: '总量',
-            areaStyle: {normal: {}},
-            data:[120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-            name:'联盟广告',
-            type:'line',
-            stack: '总量',
-            areaStyle: {normal: {}},
-            data:[220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-            name:'视频广告',
-            type:'line',
-            stack: '总量',
-            areaStyle: {normal: {}},
-            data:[150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-            name:'直接访问',
-            type:'line',
-            stack: '总量',
-            areaStyle: {normal: {}},
-            data:[320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-            name:'搜索引擎',
-            type:'line',
+            name: 'Randoop',
+            type: 'line',
             stack: '总量',
             label: {
                 normal: {
@@ -76,8 +48,60 @@ var option = {
                     position: 'top'
                 }
             },
-            areaStyle: {normal: {}},
-            data:[820, 932, 901, 934, 1290, 1330, 1320]
+            // areaStyle: {normal: {}},
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: 'Evosuite',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            // areaStyle: {normal: {}},
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: 'human1',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            // areaStyle: {normal: {}},
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: 'human2',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            // areaStyle: {normal: {}},
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: 'human3',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            // areaStyle: {normal: {}},
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
         }
     ]
 };
---
+++ js/setting-table.js
@@ -0,0 +1,57 @@
+count = 0;
+
+function addRow() {
+    if ($("#setting-id").val() != "" && $("#setting-subject").val() != "" &&
+        $("#setting-tool").val() != "" && $("#setting-time").val() != "") {
+        count++;
+        $("#setting-table").append("<tr id=\"setting-table-tr-" + count + "\">\n" +
+            "                    <td id=\"setting-id-" + count + "\">" + $("#setting-id").val() + "</td>\n" +
+            "                    <td id=\"setting-subject-" + count + "\">" + $("#setting-subject").val() + "</td>\n" +
+            "                    <td id=\"setting-tool-" + count + "\">" + $("#setting-tool").val() + "</td>\n" +
+            "                    <td id=\"setting-time-" + count + "\">" + $("#setting-time").val() + "</td>\n" +
+            "                    <td><button class=\"button-red\" id=\"delete-button" + count + "\" onclick=\"deleteRow()\">删除任务</button></td>\n" +
+            "                    <td id=\"setting-choose-button-" + count + "\"><button class=\"button-blue\" id=\"choose-button" + count + "\" onclick=\"choose()\">选入任务组</button></td>\n" +
+            "                </tr>");
+        $("#setting-id").val("");
+        $("#setting-subject").val("");
+        $("#setting-tool").val("");
+        $("#setting-time").val("");
+        $("#setting-tip").text("");
+    } else {
+        $("#setting-tip").text("请将信息填写完整");
+    }
+}
+
+function deleteRow() {
+    var current = event.target.id.substr(13);
+    $("#setting-table-tr-" + current).remove();
+}
+
+function choose() {
+    var current = event.target.id.substr(13);
+
+    $("#task-table").append("<tr>\n" +
+        "                    <td>" + $("#setting-id-" + current).text() + "</td>\n" +
+        "                    <td>" + $("#setting-subject-" + current).text() + "</td>\n" +
+        "                    <td>" + $("#setting-tool-" + current).text() + "</td>\n" +
+        "                    <td>" + $("#setting-time-" + current).text() + "</td>\n" +
+        "                </tr>");
+    var detachButton = $("#choose-button" + current).remove();
+}
+
+function emptyTask() {
+    $("#task-table").empty();
+    $("#task-table").append("<tr>\n" +
+        "                    <th>ID</th>\n" +
+        "                    <th>subject</th>\n" +
+        "                    <th>tool</th>\n" +
+        "                    <th>time_budget</th>\n" +
+        "                </tr>");
+
+    for (var i = 1; i <= count; i++) {
+        console.log($("#setting-choose-button-" + i).children().length);
+        if ($("#setting-choose-button-" + i).children().length === 0) {
+            $("#setting-choose-button-" + i).append("<button class=\"button-blue\" id=\"choose-button" + count + "\" onclick=\"choose()\">选入任务组</button>")
+        }
+    }
+}
